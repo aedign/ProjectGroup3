c20397b5c050f8b7e55fc32d184e03dc511cc35e
Andres Di Gregorio <aedign@icloud.com>
2020-11-20 14:47:29 -0500

    First commit

---
+++ Project.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+public class Project
+{
+    private string name;
+    private string description;
+    private List<string> members;
+    private List<string> functionalRequirements;
+    private List<string> nonFunctionalRequirements;
+    private List<Risk> risks;
+
+    public Project(string name, string description)
+    {
+        this.name = name;
+        this.description = description;
+        this.members = new List<string>();
+    }
+
+    public string getName()
+    {
+        return this.name;
+    }
+
+    public string getDescription()
+    {
+        return this.description;
+    }
+
+    public List<string> getMembers()
+    {
+        return this.members;
+    }
+
+    public List<string> getFunctionalRequirements()
+    {
+        return this.functionalRequirements;
+    }
+
+    public List<string> getNonFunctionalRequirements()
+    {
+        return this.nonFunctionalRequirements;
+    }
+
+    public List<Risk> getRisks()
+    {
+        return this.risks;
+    }
+
+    public void setName(string newName)
+    {
+        this.name = newName;
+    }
+
+    public void setDescription(string newDescription)
+    {
+        this.description = newDescription;
+    }
+
+    public void addMember(string name)
+    {
+        this.members.Add(name);
+    }
+
+    public void addFunctionalRequirement(string funcReq)
+    {
+        this.functionalRequirements.Add(funcReq);
+    }
+
+    public void addNonFunctionalRequirement(string nonFuncReq)
+    {
+        this.functionalRequirements.Add(nonFuncReq);
+    }
+
+    public void addRisk(Risk risk)
+    {
+        this.risks.Add(risk);
+    }
+
+    public void removeMember(string member)
+    {
+        this.members.Remove(member);
+    }
+}
---
+++ Risk.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+public class Risk
+{
+    private string description;
+    private RiskProbability probability;
+
+    public Risk(string description, RiskProbability probability)
+    {
+        this.description = description;
+        this.probability = probability;
+    }
+
+    public string getDescription()
+    {
+        return this.description;
+    }
+
+    public string getProbability()
+    {
+        return this.probability.ToString();
+    }
+
+    public void setDescription(string newDescription)
+    {
+        this.description = newDescription;
+    }
+
+    public void setProbability(RiskProbability newProbability)
+    {
+        this.probability = newProbability;
+    }
+}
